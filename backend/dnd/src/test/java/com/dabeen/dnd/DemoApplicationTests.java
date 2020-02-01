c4e48ab3060a0f40c3535ff4f3989beae327895a
Lihess <she6569@gmail.com>
2020-02-01 11:42:18 +0900

    기본설정 :  프로젝트 생성

---
+++ backend/dnd/src/test/java/com/dabeen/dnd/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dabeen.dnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
