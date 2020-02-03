84c714a0b570263fbb0a013a58b689c53df1ff69
Lihess <she6569@gmail.com>
2020-02-03 17:21:42 +0900

    테스트 : 테스트를 위한 어노테이션 추가

--- backend/dnd/src/test/java/com/dabeen/dnd/DemoApplicationTests.java
+++ backend/dnd/src/test/java/com/dabeen/dnd/DemoApplicationTests.java
@@ -1,10 +1,13 @@
 package com.dabeen.dnd;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test; 
+import org.junit.runner.RunWith; 
+import org.springframework.boot.test.context.SpringBootTest; 
+import org.springframework.test.context.junit4.SpringRunner; 
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
