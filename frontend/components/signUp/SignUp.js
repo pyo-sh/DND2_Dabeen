2b03d5fc1444af9d613e57f52394296fa8fb0067
pyo-sh <pygosky@gmail.com>
2020-02-07 17:52:06 +0900

    회원가입 페이지 40%

--- frontend/components/signUp/SignUp.js
+++ frontend/components/signUp/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 
-import { Input, Button } from 'antd';
+import { Input, Button, Select } from 'antd';
 
 const SignUpUpperDiv = styled.div`
     display: flex;
@@ -10,8 +10,7 @@ const SignUpUpperDiv = styled.div`
     justify-content: center;
     border: 1px solid #d9d9d9;
     
-    margin: auto;
-    margin-top: 90px;
+    margin: 90px auto;
     padding: 40px;
     width: 100%;
     height: 1120px;
@@ -47,19 +46,21 @@ const SignUpGetDataDiv = styled.div`
         max-width: 420px;
         min-width: 260px;
     }
-    & .Regist {
-        display: flex;
-        flex-direction: row;
-    }
-    & .Regist_Input {
-        margin: 5px;
-        max-width: 200px;
-        min-width: 120px;
+    & .Birth {
+        & div{
+            
+        }
+        & .Birth-Year{
+            width: 140px;
+        }
+        & .Birth-Month{
+            width: 100px;
+        }
+        & .Birth-Day{
+            width: 100px;
+        }
     }
     
-    & div {
-        padding: 5px;
-    }
     & .Title{
 
     }
@@ -73,19 +74,21 @@ const SignUpGetDataDiv = styled.div`
 
 // 회원가입 창
 const SignUp = () => {
+    // 현재 날짜가 필요할 거 같아서..
+    const nowDate = new Date();
     // 로그인하는데 유저의 필요한 정보의 state
     const [id, setId] = useState('');   // 아이디 state
     const [password, setPassword] = useState('');   // 비밀번호 state
     const [passwordCheck, setPasswordCheck] = useState(''); // 비밀번호 확인 state
     const [nickname, setNickname] = useState('');   // 닉네임 state
     const [name, setName] = useState('');   // 이름 state
-    const [firstRegistrationNumber, setFirstRegistrationNumber] = useState(''); // 주민등록번호 앞자리 state
-    const [secondRegistrationNumber, setSecondRegistrationNumber] = useState('');   // 주민등록번호 뒷자리 state
+    const [birthYear, setBirthYear] = useState(nowDate.getFullYear()); // 생년월일 중 년
+    const [birthMonth, setBirthMonth] = useState(nowDate.getMonth());   // 생년월일 중 월
+    const [birthDay, setBirthDay] = useState(nowDate.getDate());
     const [email, setEmail] = useState(''); // 이메일 state
     const [telephone, setTelephone] = useState('');
     const [mainAddress, setMainAddress] = useState('');
     const [subAddress, setSubAddress] = useState('');
-
 
     // 입력한 정보가 맞는 정보인지 확인하는 state
     const [isIdCorrect, setIsIdCorrect] = useState(false);  // 아이디 확인
@@ -94,72 +97,60 @@ const SignUp = () => {
     const [isNicknameCorrect, setIsNicknameCorrect] = useState(false);  // 닉네임 확인
     const [isRegistrationCorrect, setIsRegistrationCorrect] = useState(false);  // 이름과 주민등록번호 매칭 확인
     const [isEmailCorrect, setIsEmailCorrect] = useState(false);  // 아이디 확인
+    
+    // 문자열내용을 전달하기 위함
+    const check_num = /[0-9]/g; // 숫자
+    const check_eng = /[a-z|A-Z]/g; // 문자
+    const check_spc = /[~!@#$%^&*()_+|<>?:{}]/g; // 특수문자
+    const check_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g; // 한글
+    const check_spa = /(\s)/g;             // 공백
 
-    // 아이디 입력창이 바뀔 때 마다 실행하는 함수
-    const onChangeId = useCallback((e) => {
-        const targetString = e.target.value;    // Input창의 value 값
-        const deleteSpace = e.target.value.replace(/(\s*)/g,"");    // value의 공백을 제거한 값
-        if(!targetString)                 setIsIdCorrect(false);    // value가 빈 값일 때
-        else if(targetString !== deleteSpace)   setIsIdCorrect(false);  // value에 공백을 포함했을 때
-        else                                    setIsIdCorrect(true);   // 나머지는 가능한 상황
-        // state를 바뀔때마다 setState
-        setId(e.target.value);
-    }, []);
-    // 비밀번호 입력창이 바뀔 때 마다 실행하는 함수
-    const onChangePassword = useCallback((e) => {
-        const targetString = e.target.value;    // Input창의 value 값
-        const deleteSpace = e.target.value.replace(/(\s*)/g,"");    // value의 공백을 제거한 값
-        if(!targetString)                 setIsPasswordCorrect(false);  // value가 빈 값일 때
-        else if(targetString !== deleteSpace)   setIsPasswordCorrect(false);    // value에 공백을 포함했을 때
-        else                                    setIsPasswordCorrect(true); // 나머지는 가능한 상황
-        // state를 바뀔때마다 setState
-        setPassword(e.target.value);
-    }, []);
-    // 비밀번호 확인 입력창이 바뀔 때 마다 실행하는 함수.
-    const onChangePasswordCheck = useCallback((e) => { setPasswordCheck(e.target.value); }, []);
-    // 닉네임 입력창이 바뀔 때 마다 실행하는 함수
-    const onChangeNickname = useCallback((e)=>{
-        const targetString = e.target.value;    // Input창의 value 값
-        const deleteSpace = e.target.value.replace(/(\s*)/g,"");    // value의 공백을 제거한 값
-        if(!targetString)                 setIsNicknameCorrect(false);  // value가 빈 값일 때
-        else if(targetString !== deleteSpace)   setIsNicknameCorrect(false);    // value에 공백을 포함했을 때
-        else                                    setIsNicknameCorrect(true); // 나머지는 가능한 상황
-        // state를 바뀔때마다 setState
-        setNickname(targetString);
-    }, []);
-    // 이름 입력창이 바뀔 때 마다 실행하는 함수
-    const onChangeName = useCallback((e) => {
-        
-        setName(e.target.value);
-    }, []);
+    // 생년월일 선택지를 render 하기 위함
+    const yearOptions = Array(80)
+        .fill(0)
+        .map((v, index) => (
+            <Select.Option className="Month-Item" key={`${index}`}>
+                {nowDate.getFullYear() - index}
+            </Select.Option>
+        )
+    );
+    const monthOptions = Array(12)
+        .fill(0)
+        .map((v, index) => (
+            <Select.Option className="Month-Item" key={`${index}`}>
+                {index+1}
+            </Select.Option>
+        )
+    );
+    const dayOptions = Array(31)
+        .fill(0)
+        .map((v, index) => {
+        return (
+            <Select.Option className="Month-Item" key={`${index}`}>
+                {index+1}
+            </Select.Option>
+        );
+    });
 
-    // 주민등록번호 앞자리 입력창이 바뀔 때 마다 실행하는 함수
-    // const onChangeFirstRegistrationNumber = useCallback((e) => {
-    //     const targetString = e.target.value;    // Input창의 value 값
-    //     const intCheck = targetString.replace(/[0-9]/g, "");    // value값에서 숫자들을 제외한 string.
-    //     if(!targetString) setFirstRegistrationNumber(e.target.value);   // value가 빈 값일 때
-    //     else if(!intCheck) {  // 숫자들을 제외한 string 값이 비어있으면 string에 숫자만 있는 것이다.
-    //         setFirstRegistrationNumber(e.target.value); // 그러면 state값을 바꾸게 해준다.
-    //     }
-    //     else    console.log("숫자만 입력해");   // 숫자가 아니면 state값을 바꿀 수 없게 만든다.
-    // }, []);
-    // // 주민등록번호 뒷자리 입력창이 바뀔 때 마다 실행하는 함수
-    // const onChangeSecondRegistrationNumber = useCallback((e) => {
-    //     console.log(e.target.value);
-    //     setSecondRegistrationNumber(e.target.value)
-    // }, []);
-
-    // 아이디 입력창이 바뀔 때 마다 실행하는 함수
-    const onChangeEmail = useCallback((e) => {
+    // 아이디 = 허용 : [영어 / 숫자], 불가 : [공백, 한글, 특수문자]
+    // 비밀번호 = 허용 : [영어 / 숫자 / 특수문자], 불가 : [공백, 한글]
+    // 닉네임 = 허용 : [영어 / 한글 / 숫자], 불가 : [특수문자, 공백]
+    // 이름 = 허용 : [영어 / 한글 / 공백], 불가 : [특수문자, 숫자]
+    // 이메일 = 허용 : [영어 / 숫자 / @], 불가 : [특수문자, 한글, 공백]
+    // 전화번호 = 허용 : [숫자], 불가 : [나머지]
+    // 주소 = 허용 : [한글 / 숫자 / 영어 / 공백 / ,.:], 불가 : [특수문자]
+    // setState는 지정, checkObject는 허용하는 문자들을 배열로 넣어준다.
+    const onChangeInput = (setState, checkObject) => useCallback((e) => {
         const targetString = e.target.value;    // Input창의 value 값
-        const deleteSpace = e.target.value.replace(/(\s*)/g,"");    // value의 공백을 제거한 값
-        if(!targetString)                 setIsEmailCorrect(false);    // value가 빈 값일 때
-        else if(targetString !== deleteSpace)   setIsEmailCorrect(false);  // value에 공백을 포함했을 때
-        else                                    setIsEmailCorrect(true);   // 나머지는 가능한 상황
-        // state를 바뀔때마다 setState
-        setEmail(e.target.value);
+        let deleteString = targetString;
+        // checkObject들을 모두 지운다.
+        checkObject.forEach(element => {
+            deleteString = deleteString.replace(element, "");
+        });
+        // 지운 값이 비어있다면?? setState
+        if(!deleteString)
+            setState(e.target.value);
     }, []);
-
 
     // 비밀번호와 비밀번호 확인 state가 바뀔 때 마다 확인
     useEffect(() => {
@@ -167,22 +158,21 @@ const SignUp = () => {
         (password === passwordCheck)    ?   setIsPasswordChecked(true)  :   setIsPasswordChecked(false);
     }, [password, passwordCheck]);
 
-    // 이름과 주민번호 확인 state가 바뀔 때 마다 확인
-    useEffect(()=> {
-        // 일단 조건 없이 true로 해놓았음.
-        setIsRegistrationCorrect(true);
-    }, [name, firstRegistrationNumber, secondRegistrationNumber]);
-
     // 가입하기 버튼 눌렀을 때 값을 전달하기 위한 함수
     const onClickSignUp = useCallback((e) => {
         if(isNicknameCorrect && isIdCorrect && isPasswordCorrect && isPasswordChecked && isRegistrationCorrect){
             const userLog = {
-                nickname: nickname,
-                id: id,
-                password: password,
-                name: name,
-                firstRegistrationNumber: firstRegistrationNumber,
-                secondRegistrationNumber: secondRegistrationNumber
+                id,
+                password,
+                nickname,
+                name,
+                birthYear,
+                birthMonth,
+                birthDay,
+                email,
+                telephone,
+                mainAddress,
+                subAddress,
             }
             console.dir(userLog);
             return userLog;
@@ -195,8 +185,14 @@ const SignUp = () => {
         id,
         password,
         name,
-        firstRegistrationNumber,
-        secondRegistrationNumber,
+        birthYear,
+        birthMonth,
+        birthDay,
+        email,
+        telephone,
+        mainAddress,
+        subAddress,
+
         isNicknameCorrect,
         isIdCorrect,
         isPasswordCorrect,
@@ -210,7 +206,8 @@ const SignUp = () => {
                 <div>아이디 *</div>
                 <Input
                     placeholder="아이디 입력"
-                    onChange={onChangeId}
+                    onChange={onChangeInput(setId, [check_eng, check_num])}
+                    value={id}
                 />
                 {isIdCorrect || (id === '')
                 ?   <div className="Check"></div>
@@ -221,7 +218,9 @@ const SignUp = () => {
                 <div>비밀번호 *</div>
                 <Input.Password
                     placeholder="비밀번호(8~20자리)"
-                    onChange={onChangePassword}
+                    onChange={onChangeInput(setPassword, [check_eng, check_num, check_spc])}
+                    value={password}
+                    maxLength={20}
                 />
                 {isPasswordCorrect || (password === '')
                 ?   <div className="Check"></div>
@@ -232,7 +231,9 @@ const SignUp = () => {
                 <div>비밀번호 확인 *</div>
                 <Input.Password
                     placeholder="비밀번호 재입력"
-                    onChange={onChangePasswordCheck}
+                    onChange={onChangeInput(setPasswordCheck, [check_eng, check_num, check_spc])}
+                    value={passwordCheck}
+                    maxLength={20}
                 />
                 {isPasswordChecked
                 ?   <div className="Check"></div>
@@ -243,7 +244,8 @@ const SignUp = () => {
                 <div className="Title">닉네임 *</div>
                 <Input
                     placeholder="닉네임 입력"
-                    onChange={onChangeNickname}
+                    onChange={onChangeInput(setNickname, [check_eng, check_num, check_kor])}
+                    value={nickname}
                 />
                 {isNicknameCorrect || (nickname === '')
                 ?   <div className="Check"></div>
@@ -254,54 +256,55 @@ const SignUp = () => {
                 <div>이름 *</div>
                 <Input
                     placeholder="이름 입력"
-                    onChange={onChangeName}
+                    onChange={onChangeInput(setName, [check_eng, check_kor, check_spa])}
+                    value={name}
                 />
             </SignUpGetDataDiv>
             <SignUpGetDataDiv>
                 <div>생년월일 *</div>
-                <div className="Regist">
-                    <Input
-                        className="Regist_Input"
-                        placeholder=""
-                        value={firstRegistrationNumber}
-                        maxLength={6}
-                    />
-                    <div>-</div>
-                    <Input
-                        type="number"
-                        className="Regist_Input"
-                        placeholder=""
-                        maxLength={7}
-                    />
+                <div className="Birth">
+                    <Select className="Birth-Year">
+                        {yearOptions}
+                    </Select>
+                    <Select className="Birth-Month">
+                        {monthOptions}
+                    </Select>
+                    <Select className="Birth-Day">
+                        {dayOptions}
+                    </Select>
                 </div>
                 {isRegistrationCorrect
                     ?   <div className="Check"></div>
-                    :   <div className="Check">이름과 주민등록번호를 확인해주세요</div>
+                    :   <div className="Check">이름과 생년월일을 확인해주세요</div>
                 }
             </SignUpGetDataDiv>
             <SignUpGetDataDiv>
                 <div>이메일 *</div>
                 <Input
                     placeholder="이메일 입력"
-                    // onChange={onChangeId}
+                    onChange={onChangeInput(setEmail, [check_eng, check_num, /[@]/g])}
+                    value={email}
                 />
             </SignUpGetDataDiv>
             <SignUpGetDataDiv>
                 <div>전화번호 *</div>
                 <Input
                     placeholder="'-'를 제외한 숫자"
-                    // onChange={onChangeId}
+                    onChange={onChangeInput(setTelephone, [check_num])}
+                    value={telephone}
                 />
             </SignUpGetDataDiv>
             <SignUpGetDataDiv>
                 <div>주소 *</div>
                 <Input
                     placeholder="시"
-                    // onChange={onChangeId}
+                    onChange={onChangeInput(setMainAddress, [check_eng, check_num, check_kor, /[,.:;'"]/g])}
+                    value={mainAddress}
                 />
                 <Input
                     placeholder="면/읍/리?"
-                    // onChange={onChangeId}
+                    onChange={onChangeInput(setSubAddress, [check_eng, check_num, check_kor, check_spa, /[,.:;'"]/g])}
+                    value={subAddress}
                 />
             </SignUpGetDataDiv>
             <Button
