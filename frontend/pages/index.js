ae39029f072df5aa0f2e040e10f02325f00563cc
pyo-sh <pygosky@gmail.com>
2020-02-09 16:30:30 +0900

    index

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Main from "../components/main/Main";
 import PostWrite from "../components/posts/PostWrite";
 
@@ -7,37 +6,6 @@ const Home = () => {
   return (
     <>
       <Main/>
-=======
-import styled from 'styled-components';
-import MenuBar from "../components/main/MenuBar";
-import MainBottom from "../components/main/MainBottom";
-// import PostWrite from "../components/posts/PostWrite";
-import RegistSupplier from "../components/signUp/RegistSupplier";
-
-const Content = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 15px;
-`;
-
-const Footer = styled.div`
-  position: relative;
-  bottom: 0;
-  padding-top: 10px;
-  border-top: solid 1px darkgray;
-  width: 100%;
-  color: gray;
-`;
-
-const Home = () => {
-  return (
-    <>
-    <MenuBar />
-    <RegistSupplier/>
-    <Footer><MainBottom /></Footer>
->>>>>>> pyo
     </>
   );
 };
