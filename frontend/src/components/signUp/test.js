90d2899b2ab6dff1c3f5b7f5c3a1e48442d353f4
Lihess <she6569@gmail.com>
2020-02-14 02:24:28 +0900

    수정 : gitignore 적용

--- frontend/src/components/signUp/test.js
+++
@@ -1,6 +0,0 @@
-const test = ""
-const intCheck = test.replace(/[0-9]/g, "");
-
-// var regex = /^[0-9]$/g;
-// console.log(regex.test(test))
-console.log(!intCheck);
--- frontend/src/pages/signup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SignUp from '../components/signUp/SignUp';
-const signupMain = () => {
-    return (
-        <SignUp/>
-    );
-};
-
-export default signupMain;
