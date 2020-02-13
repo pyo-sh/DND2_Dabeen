164bd6e2c7efd186dd07597432a96158115e71c1
Lihess <she6569@gmail.com>
2020-02-13 16:25:56 +0900

    배포 : 수정

--- backend/dnd/src/main/java/com/dabeen/dnd/config/WebMvcConfigurerImpl.java
+++ backend/dnd/src/main/java/com/dabeen/dnd/config/WebMvcConfigurerImpl.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfigurerImpl implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**") //모든 요청에 대해서
-                .allowedOrigins("http://localhost:3000"); // 3000포트만 허용
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+
     }
 }
