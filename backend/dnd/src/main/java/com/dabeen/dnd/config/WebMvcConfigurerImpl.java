28d29683d8331a77dbd7a4d23cd5c1ba8032dcf4
Lihess <she6569@gmail.com>
2020-02-14 18:08:05 +0900

    수정

--- backend/dnd/src/main/java/com/dabeen/dnd/config/WebMvcConfigurerImpl.java
+++ backend/dnd/src/main/java/com/dabeen/dnd/config/WebMvcConfigurerImpl.java
@@ -3,9 +3,11 @@
 
 package com.dabeen.dnd.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfigurerImpl implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
