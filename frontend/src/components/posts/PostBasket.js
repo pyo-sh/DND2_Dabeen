b61dceb037cf707a97deb6568e429383ac7646fe
geonwoomun <ansejrrhkd@naver.com>
2020-02-18 21:43:24 +0900

    반응형 없애고 싶다..!!

--- frontend/src/components/posts/PostBasket.js
+++ frontend/src/components/posts/PostBasket.js
@@ -25,7 +25,6 @@ const PostBasket = () => {
 
 const PostBasketUpperDiv = styled.div`
     width: 100%;
-    margin: 100px 0;
     display: flex;
     flex-direction: column;
     align-items: center;
