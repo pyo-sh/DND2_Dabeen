909fccd089dc1b5a0ff48d6c84290f1806a1f49f
JerryKwon <kwonyoung234@gmail.com>
2020-02-20 20:24:02 +0900

    사용하지 않는 slfj4 삭제

--- backend/dnd/src/test/java/com/dabeen/dnd/repository/PostRepositoryTest.java
+++ backend/dnd/src/test/java/com/dabeen/dnd/repository/PostRepositoryTest.java
@@ -5,8 +5,6 @@ package com.dabeen.dnd.repository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
