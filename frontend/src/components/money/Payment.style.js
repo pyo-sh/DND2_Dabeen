1cb0f437d040df5080855d9b9e7607715590f07a
geonwoomun <ansejrrhkd@naver.com>
2020-02-26 20:27:18 +0900

    오타 수정!

--- frontend/src/components/money/Payment.style.js
+++ frontend/src/components/money/Payment.style.js
@@ -1,5 +1,5 @@
 import {Modal} from 'antd';
-import styeld from 'styled-components';
+import styled from 'styled-components';
 
 export const PaymentModal = styled(Modal)`
     & .ant-modal-header div {
