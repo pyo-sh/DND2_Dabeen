96c4f56e6a60b32e1b2bab43acd7cd3e577f1590
pyo-sh <pygosky@gmail.com>
2020-03-01 20:04:05 +0900

    MyLocation : Map 추가

--- frontend/src/components/map/MyLocation.styled.js
+++ frontend/src/components/map/MyLocation.styled.js
@@ -6,3 +6,10 @@ export const MarkImage = styled.img`
     margin-top: -33.5px;
     margin-left: -15px;
 `;
+
+export const Map = styled.div`
+    width: 100%;
+    max-width: 550px;
+    min-width: 250px;
+    height: 200px;
+`;
