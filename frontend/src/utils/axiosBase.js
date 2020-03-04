7cb9d9eaae4fd01f026794d237a352dd72e04181
geonwoomun <ansejrrhkd@naver.com>
2020-03-05 02:23:47 +0900

    https랑 포트 붙여서 다시 시도!

--- frontend/src/utils/axiosBase.js
+++ frontend/src/utils/axiosBase.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customAxios = axios.create({
-    baseURL : 'https://15.164.2.26:3307/api'
+    baseURL : 'https://www.dabeen.org:3307/api'
 });
-
+// https://15.164.2.26:3307
 export default customAxios;
